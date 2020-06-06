1d017e14072ea26df9b69b58ebfc7e4b0777a893
Devin Riegle <riegledevin@gmail.com>
2020-06-06 18:44:28 -0400

    Add /api/users/currentuser endpoint and ingress srv config

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -5,4 +5,8 @@ const app = express();
 
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('hi there');
+});
+
 app.listen(3000, () => console.log('@ticketing/auth listening on port 3000'));
