8458f30ed0a7046d7343adfe52cc1fb1413175bb
Devin Riegle <riegledevin@gmail.com>
2020-06-07 18:10:09 -0400

    works

--- packages/auth/src/test/setup.ts
+++ packages/auth/src/test/setup.ts
@@ -2,11 +2,27 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '@ticketing/auth/src/app';
 
+let mongo: MongoMemoryServer;
+
 beforeAll(async () => {
-  const mongo = new MongoMemoryServer();
+  mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
   await mongoose.connect(mongoUri, {
-
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
 });
