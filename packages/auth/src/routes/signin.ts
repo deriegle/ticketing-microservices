73f9fff7f8c23b6a5ea8f49a04cf6bd87aacae8b
Devin Riegle <riegledevin@gmail.com>
2020-06-07 17:13:42 -0400

    Add signin logic

--- packages/auth/src/routes/signin.ts
+++ packages/auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import {Router, Request, Response} from 'express';
 import { body } from 'express-validator';
 import { validateRequest } from '@ticketing/auth/src/middleware/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { PasswordService } from '../services/password-service';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
@@ -11,7 +15,33 @@ router.post(
     body('password').trim().notEmpty().withMessage('Password is required'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({
+      email,
+    })
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid email or password');
+    }
+
+    if (!await PasswordService.compare(existingUser.password, password)) {
+      throw new BadRequestError('Invalid email or password');
+    }
+
+    const userJwt = jwt.sign({
+      userId: existingUser._id,
+      email: existingUser.email,
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+      jwt: userJwt,
+    } as any;
+
+    return res.status(200).json({
+      user: existingUser,
+      token: userJwt,
+    })
   },
 );
 
