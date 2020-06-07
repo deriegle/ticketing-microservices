92cfd24dfcc476da9527885e556ca61ab1125f83
Devin Riegle <riegledevin@gmail.com>
2020-06-07 11:09:29 -0400

    add mongoose types and connect to db

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import { currentUserRouter } from '@ticketing/auth/src/routes/current-user';
-import { signInRouter } from '@ticketing/auth/src/routes/signin';
-import { signOutRouter } from '@ticketing/auth/src/routes/signout';
-import { signUpRouter } from '@ticketing/auth/src/routes/signup';
-import { errorHandler } from '@ticketing/auth/src/middleware/error-handler';
-import { NotFoundError } from '@ticketing/auth/src/errors/not-found-error';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+
+import { currentUserRouter } from "@ticketing/auth/src/routes/current-user";
+import { signInRouter } from "@ticketing/auth/src/routes/signin";
+import { signOutRouter } from "@ticketing/auth/src/routes/signout";
+import { signUpRouter } from "@ticketing/auth/src/routes/signup";
+import { errorHandler } from "@ticketing/auth/src/middleware/error-handler";
+import { NotFoundError } from "@ticketing/auth/src/errors/not-found-error";
 
 const app = express();
 
@@ -17,10 +19,24 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-app.all('*', async () => {
-  throw new NotFoundError()
+app.all("*", async () => {
+  throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
-app.listen(3000, () => console.log('@ticketing/auth listening on port 3000'));
+const init = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+
+  app.listen(3000, () => console.log("@ticketing/auth listening on port 3000"));
+};
+
+init();
