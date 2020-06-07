6f374973913ff0b21d0eaaeb9759eb380b20f873
Devin Riegle <riegledevin@gmail.com>
2020-06-07 16:28:32 -0400

    Add jsonwebtoken and cookie session

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from "@ticketing/auth/src/routes/current-user";
 import { signInRouter } from "@ticketing/auth/src/routes/signin";
@@ -12,7 +13,12 @@ import { NotFoundError } from "@ticketing/auth/src/errors/not-found-error";
 
 const app = express();
 
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true,
+}));
 
 app.use(currentUserRouter);
 app.use(signInRouter);
