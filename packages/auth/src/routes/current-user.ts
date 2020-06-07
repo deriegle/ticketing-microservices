4d95fa43eb68dbb6ad1cb6c4d59ca812afe79447
Devin Riegle <riegledevin@gmail.com>
2020-06-07 17:21:03 -0400

    add current user route

--- packages/auth/src/routes/current-user.ts
+++ packages/auth/src/routes/current-user.ts
@@ -1,9 +1,30 @@
 import {Router} from 'express';
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user';
 
 const router = Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('hello, there');
-});
+router.get(
+  '/api/users/currentuser',
+  async (req, res) => {
+    if (!req.session?.jwt) {
+      return res.status(400).json({
+        currentUser: null,
+      });
+    }
+
+    try {
+      const payload =  jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+      return res.status(200).json({
+        currentUser: payload,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        currentUser: null,
+      });
+    }
+  }
+);
 
 export const currentUserRouter = router;
