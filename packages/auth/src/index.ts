883635c6c47060b15d16839b58f26719d2e77098
Devin Riegle <riegledevin@gmail.com>
2020-06-08 23:59:23 -0400

    works

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -3,10 +3,10 @@ import { EnvvarService } from "@ticketing/backend-core";
 import { app } from "./app";
 
 const main = async () => {
-  EnvvarService.validateEnvvars(["JWT_KEY"]);
+  EnvvarService.validateEnvvars(["JWT_KEY", "MONGO_URI"]);
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
--- packages/backend-core/src/types/environment.d.ts
+++ packages/backend-core/src/types/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       JWT_KEY: string;
+      MONGO_URI: string;
     }
   }
 }
