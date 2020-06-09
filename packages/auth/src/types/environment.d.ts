c86de7b16b368512a938e0f8dc544e58b1a49d69
Devin Riegle <riegledevin@gmail.com>
2020-06-08 22:58:03 -0400

    Add shared code

---
+++ packages/auth/src/types/environment.d.ts
@@ -0,0 +1,15 @@
+interface CurrentUserPayload {
+  id: string;
+  password: string;
+  iat: number;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: CurrentUserPayload;
+    }
+  }
+}
+
+export { CurrentUserPayload };
