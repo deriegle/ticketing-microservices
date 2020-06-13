b8de20b2224a8133e5042e65eb8e2534532f25d1
Devin Riegle <riegledevin@gmail.com>
2020-06-13 00:05:43 -0400

    Add orders service

---
+++ packages/orders/src/index.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { EnvvarService } from "@ticketing/backend-core";
+import { natsWrapper } from "@ticketing/tickets/src/nats-wrapper";
+
+const main = async () => {
+  EnvvarService.validateEnvvars([
+    "JWT_KEY",
+    "MONGO_URI",
+    "NATS_CLUSTER_ID",
+    "NATS_URL",
+    "NATS_CLIENT_ID",
+  ]);
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+
+  app.listen(3000, () =>
+    console.log("@ticketing/tickets listening on port 3000")
+  );
+};
+
+main();
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      JWT_KEY: string;
+      MONGO_URI: string;
+      NATS_CLUSTER_ID: string;
+      NATS_URL: string;
+      NATS_CLIENT_ID: string;
+    }
+  }
+}
