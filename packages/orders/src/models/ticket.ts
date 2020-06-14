7874acce228b74eba992b3da967976e28597b050
Devin Riegle <riegledevin@gmail.com>
2020-06-13 22:28:31 -0400

    works

--- packages/orders/src/models/ticket.ts
+++ packages/orders/src/models/ticket.ts
@@ -1,20 +1,57 @@
-import { createModel } from "@ticketing/backend-core";
-import { Document } from "mongoose";
+import { createModel, OrderStatus } from "@ticketing/backend-core";
+import { Document, Model, Schema, model } from "mongoose";
+import { Order } from "./order";
 
 interface TicketAttributes {
   title: string;
   price: number;
 }
 
-export type TicketDocument = Document & TicketAttributes;
+export type TicketDocument = Document &
+  TicketAttributes & {
+    isReserved: () => Promise<boolean>;
+  };
 
-export const Ticket = createModel<TicketAttributes, TicketDocument>("Ticket", {
-  title: {
-    type: String,
-    required: true,
+type TicketModel = Model<TicketDocument>;
+
+const ticketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      versionKey: false,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.methods.isReserved = async function (): Promise<boolean> {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!existingOrder;
+};
+
+export const Ticket = model<TicketDocument, TicketModel>(
+  "Ticket",
+  ticketSchema
+);
--- packages/orders/src/routes/new.ts
+++ packages/orders/src/routes/new.ts
@@ -1,9 +1,22 @@
 import { Response, Request, Router } from "express";
 import mongoose from "mongoose";
-import { requireAuth, validateRequest } from "@ticketing/backend-core";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  BadRequestError,
+  OrderStatus,
+} from "@ticketing/backend-core";
 import { body } from "express-validator";
+import { Ticket } from "@ticketing/orders/src/models/ticket";
+import { Order } from "../models/order";
 
 const router = Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+interface RequestBody {
+  ticketId: string;
+}
 
 router.post(
   "/api/orders",
@@ -16,8 +29,30 @@ router.post(
       .withMessage("Ticket ID must be provided"),
   ],
   validateRequest,
-  async (req: Request, res: Response) => {
-    res.send({ order: [] });
+  async (req: Request<any, any, RequestBody>, res: Response) => {
+    const ticket = await Ticket.findById(req.body.ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    if (await ticket.isReserved()) {
+      throw new BadRequestError(
+        `Ticket ${req.body.ticketId} is already reserved.`
+      );
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = await Order.create({
+      userId: req.currentUser!.userId,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    res.status(201).send({ order });
   }
 );
 
